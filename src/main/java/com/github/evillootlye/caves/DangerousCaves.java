ae1145ff3f7cf3f19a270b15be5170f2c3d5e7db
imdanix <idanix@list.ru>
2020-02-17 21:06:44 +0300

    You're welcome

---
+++ src/main/java/com/github/evillootlye/caves/DangerousCaves.java
@@ -0,0 +1,17 @@
+package com.github.evillootlye.caves;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DangerousCaves extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		// Plugin startup logic
+
+	}
+
+	@Override
+	public void onDisable() {
+		// Plugin shutdown logic
+	}
+}
