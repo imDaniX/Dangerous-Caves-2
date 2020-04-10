1b2d244a278b5eff8d4bc7325dbc84d97ab16419
imdanix <idanix@list.ru>
2020-04-10 15:54:45 +0300

    Fix rotation issue.

--- compatibility/aqua/src/main/java/me/imdanix/caves/compatibility/FlattenedMaterials.java
+++ compatibility/aqua/src/main/java/me/imdanix/caves/compatibility/FlattenedMaterials.java
@@ -6,7 +6,9 @@ import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.block.data.BlockData;
 import org.bukkit.block.data.Directional;
+import org.bukkit.block.data.FaceAttachable;
 import org.bukkit.block.data.MultipleFacing;
+import org.bukkit.block.data.type.Switch;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.Collections;
@@ -42,7 +44,9 @@ public class FlattenedMaterials implements MaterialsProvider {
     @Override
     public void rotate(Block block, BlockFace face) {
         BlockData data = block.getBlockData();
-        if(data instanceof Directional) {
+        if(data instanceof Switch) {
+            ((Switch) data).setAttachedFace(FaceAttachable.AttachedFace.FLOOR);
+        } else if (data instanceof Directional) {
             ((Directional) data).setFacing(face);
         } else if(data instanceof MultipleFacing) {
             ((MultipleFacing) data).setFace(face, true);
